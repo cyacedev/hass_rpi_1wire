474296e72f274e6cb5da73c4b09494ec633751e6
cyace <git@cyace.dev>
2019-07-30 14:11:41 +0200

    parameterized test

--- sensor.py
+++ sensor.py
@@ -6,20 +6,25 @@ import decimal
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
-    add_devices([TempSensor()])
+    add_devices([TempSensor(config['nice_name'], config['serial_id'])])
 
 
 class TempSensor(Entity):
     """Representation of a Sensor."""
 
-    def __init__(self):
+    nice_name = "nice_name not defined"
+    serial_id = "not defined"
+
+    def __init__(self, nice_name, serial_id):
         """Initialize the sensor."""
         self._state = None
+        self.nice_name = nice_name
+        self.serial_id = serial_id
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Speicher 1 Oben'
+        return nice_name
 
     @property
     def state(self):
@@ -35,10 +40,10 @@ class TempSensor(Entity):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._state = self.readTemp("28-000005142450")
+        self._state = self.readTemp()
 
-    def readTemp(self, serial):
-        sensorPath = "/sys/bus/w1/devices/%s/w1_slave" % serial
+    def readTemp(self):
+        sensorPath = "/sys/bus/w1/devices/%s/w1_slave" % serial_id
         if os.path.isfile(sensorPath) == True:
             tfile = open(sensorPath) 
             tempText = tfile.read() 
